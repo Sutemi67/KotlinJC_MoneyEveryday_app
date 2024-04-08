a0445e2558c454ccc17d3efc6e959dc1db96cb05
Sergey Boykov <sutemi67@gmail.com>
2024-04-08 22:00:26 +0700

    general mockup

--- app/src/main/java/com/example/moneyeverydaycompose/MainActivity.kt
+++ app/src/main/java/com/example/moneyeverydaycompose/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.moneyeverydaycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.moneyeverydaycompose.ui.theme.MoneyEverydayComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,8 +34,7 @@ class MainActivity : ComponentActivity() {
             MoneyEverydayComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Income()
                 }
@@ -43,18 +47,24 @@ class MainActivity : ComponentActivity() {
 fun Income() {
     var summary by remember { mutableIntStateOf(0) }
     var input by remember { mutableStateOf("") }
-    Column (
-        Modifier.wrapContentSize(Alignment.Center)
-    ){
+    Column(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize()
+    ) {
         Text(
-            text = "$summary"
+            text = "$summary", Modifier.align(alignment = Alignment.CenterHorizontally)
         )
         Text(
-            text = input
+            text = input, Modifier.align(alignment = Alignment.CenterHorizontally)
         )
-        Row (
-            Modifier.wrapContentSize(Alignment.Center)
-        ){
+        Spacer(modifier = Modifier.height(50.dp))
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
             Column {
                 Button(onClick = { input += Button.NUMBER1.toString() }) {
                     Text(text = "1")
@@ -62,14 +72,44 @@ fun Income() {
                 Button(onClick = { input += Button.NUMBER1.toString() }) {
                     Text(text = "2")
                 }
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "3")
+                }
             }
             Column {
-                Button(onClick = {summary+=input.toInt()}) {
-                    Text(text = "Прибавить как прибыль")
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "1")
                 }
-                Button(onClick = {summary-=input.toInt()}) {
-                    Text(text = "Вычесть как расходы")
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "2")
                 }
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "2")
+                }
+            }
+            Column {
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "1")
+                }
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "2")
+                }
+                Button(onClick = { input += Button.NUMBER1.toString() }) {
+                    Text(text = "2")
+                }
+            }
+
+        }
+        Column {
+            Button(
+                onClick = { summary += input.toInt() }, Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Прибавить как прибыль")
+            }
+            Button(
+                onClick = { summary -= input.toInt() }, Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Вычесть как расходы")
             }
         }
     }
